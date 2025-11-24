10aa7924e58a55bf30ae19169f975613d770f709
Hector Ramirez <sramire3000@gmail.com>
2025-11-24 11:58:03 -0600

    Update

--- spring/java-utils/StringMasker.java
+++ spring/java-utils/StringMasker.java
@@ -3,6 +3,33 @@ package sv.com.app.util;
 
 public class StringMasker{
 	
+	public static maskString(String input){
+		
+		if (input==null || input.length() <= 4) {
+			return input;
+		}
+		
+		//Si es un correo electonica
+		if (input.contains("@")) {
+			int atIndex = input.indexOf("@");
+			String localPart  = input.substring(0, atIndex);
+			String domainPart = input.substring(atIndex);
+			
+			//Mostrar los ultimos 4 caracteres
+			int visibleChars = 2;
+			int maskLength = Math.max(0, localPart.length() - visibleChars);
+			String maskedLocal = repeatChar('*', maskLength) + localPart.substring(maskLength);
+			return maskedLocal + domainPart;
+		}
+		
+		// Si es un nuero de tarjeta o similar
+		int visibleChars = 4;
+		int maskLength = input.length() - visibleChars;
+		String maskedLocal = repeatChar('*', Math.max(0, maskLength));
+		String visible = input.substring(maskLength);
+		return masked + visible;
+	}
+	
 	public static String repeatChar(char c, int count){
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < count;i++){
